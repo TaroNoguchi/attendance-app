29ba2706dccbfc9d006417611f22c9fc2679f411
Taro Noguchi <white24collar.n.j@gmail.com>
2019-05-23 21:55:55 +0900

    Tried to create CulcurateFee class.

--- attendance/models.py
+++ attendance/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth import get_user_model
 from datetime import datetime
+from django.utils import timezone
 
 # Create your models here.
 class SubmitAttendance(models.Model):
@@ -9,12 +10,12 @@ class SubmitAttendance(models.Model):
         db_table = 'attendance'
     
     PLACES = (
-        ('TN', 'TANKS'),
-        ('RT', 'Red Tails'),
-        ('AN', 'ANNIE HALL'),
-        ('NK', 'NOKUROXY'),
-        ('HC', '七'),
-        ('AM', 'ART MON ZEN'),
+        (1, 'TANKS'),
+        (2, 'Red Tails'),
+        (3, 'ANNIE HALL'),
+        (4, 'NOKUROXY'),
+        (5, '七'),
+        (6, 'ART MON ZEN'),
     )
     IN_OUT = (
         (1, 'IN'),
@@ -22,7 +23,24 @@ class SubmitAttendance(models.Model):
     )
 
     staff = models.ForeignKey(get_user_model(), verbose_name="スタッフ", on_delete=models.CASCADE, default=None)
-    place = models.CharField(verbose_name='出勤場所名', max_length=50, choices=PLACES, default=None)
+    place = models.IntegerField(verbose_name='出勤場所名', choices=PLACES, default=None)
     in_out = models.IntegerField(verbose_name='IN/OUT', choices=IN_OUT, default=None)
-    time = models.DateTimeField(verbose_name="打刻時間")
+    time = models.TimeField(verbose_name="打刻時間")
+    date = models.DateField(verbose_name='打刻日')
+
+    
+class CulculateFee(models.Model):
+    '''
+    class Meta:
+        db_table = 'fee'
+    #date = SubmitAttendance.objects.filter('date')
+    start = SubmitAttendance.objects.get(in_out=1, staff_id=9).time
+    end = SubmitAttendance.objects.get(in_out=0, staff_id=9).time
+    howlong_hours = ((end - start).seconds) / 3600
+    fee = howlong_hours * 900 
+    def culculatedFee(self):
+        self.save()
+    '''
+
+
 
--- attendance/views.py
+++ attendance/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render, redirect
 from django.views import View
 from django.contrib.auth.mixins import LoginRequiredMixin
-from .models import SubmitAttendance
+from .models import SubmitAttendance, CulculateFee
 from .forms import SubmitAttendanceForm
 from datetime import datetime
+from django.utils import timezone
 # Create your views here.
 class IndexView(LoginRequiredMixin, View):
     def get(self, request):
@@ -23,19 +24,20 @@ class ResultView(View):
         day = now.day
         hour = now.hour
         minute = now.minute
+        
         obj = form.save(commit=False)
         obj.place = request.POST["place"]
         obj.in_out = request.POST["in_out"]
         obj.staff = request.user
-        obj.time = now
+        obj.date = datetime.now().date()
+        obj.time = datetime.now().time()
         obj.save()
         if request.POST["in_out"] == '1':
             comment = str(month) + "月" + str(day) +"日" + str(hour) + "時" + str(minute) + "分\n" + "出勤確認しました。今日も頑張りましょう！"
         else:
             comment = str(month) + "月" + str(day) +"日" + str(hour) + "時" + str(minute) + "分\n" + "退勤確認しました。お疲れ様でした(^-^)！"
         context = {
-            'place': request.POST["place"],
-            'in_out': request.POST["in_out"],
+            'place': SubmitAttendance.PLACES[int(obj.place)-1][1],
             'comment': comment,
         }
         return render(request, 'attendance/result.html', context)

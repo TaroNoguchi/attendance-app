93f0a578f74ad058980f58493ead529cfe5098ce
Taro Noguchi <white24collar.n.j@gmail.com>
2019-05-23 18:01:50 +0900

    Made relation with account management and main app.

--- attendance/models.py
+++ attendance/models.py
@@ -3,25 +3,26 @@ from django.contrib.auth import get_user_model
 from datetime import datetime
 
 # Create your models here.
-class Place(models.Model):
-    # place model  
-    class Meta:
-        db_table = 'place'
-    place = models.CharField(verbose_name='出勤場所名', max_length=50)
-
-class Staff(models.Model):
-    # logging in user model
-    class Meta:
-        db_table = 'staff'
-    staff = models.ForeignKey(get_user_model(), verbose_name="スタッフ名", on_delete=models.CASCADE)
-    
-
-
 class SubmitAttendance(models.Model):
+    
     class Meta:
         db_table = 'attendance'
-    staff = staff = models.ForeignKey(get_user_model(), verbose_name="スタッフ", on_delete=models.CASCADE)
-    place = models.ManyToManyField(Place, verbose_name='出勤場所')
-    in_out = models.CharField(verbose_name='IN/OUT',max_length=3)
-    time = models.DateTimeField(verbose_name="打刻時間", auto_now=False, auto_now_add=True)
+    
+    PLACES = (
+        ('TN', 'TANKS'),
+        ('RT', 'Red Tails'),
+        ('AN', 'ANNIE HALL'),
+        ('NK', 'NOKUROXY'),
+        ('HC', '七'),
+        ('AM', 'ART MON ZEN'),
+    )
+    IN_OUT = (
+        (1, 'IN'),
+        (0, 'OUT'),
+    )
+
+    staff = models.ForeignKey(get_user_model(), verbose_name="スタッフ", on_delete=models.CASCADE, default=None)
+    place = models.CharField(verbose_name='出勤場所名', max_length=50, choices=PLACES, default=None)
+    in_out = models.IntegerField(verbose_name='IN/OUT', choices=IN_OUT, default=None)
+    time = models.DateTimeField(verbose_name="打刻時間")
 
--- attendance/urls.py
+++ attendance/urls.py
@@ -4,4 +4,5 @@ from . import views # ←これ忘れないようにする！！
 app_name = 'attendance'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('result/', views.result, name='result'),
 ]

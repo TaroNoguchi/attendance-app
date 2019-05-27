e145684301fcd3aec6bb863393dddff02bb425cd
Taro Noguchi <white24collar.n.j@gmail.com>
2019-05-27 12:21:11 +0900

    Modified how the objects to appear on the admin page.

--- attendance/models.py
+++ attendance/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth import get_user_model
+from django.contrib.auth.models import User
 import datetime as dt
 from django.utils import timezone
 
@@ -28,6 +29,12 @@ class SubmitAttendance(models.Model):
     time = models.TimeField(verbose_name="打刻時間")
     date = models.DateField(verbose_name='打刻日')
 
+    #辞書化
+    place_dict = dict(PLACES)
+    in_out_dict = dict(IN_OUT)
+
+    def __str__(self):
+        return  User.objects.get(id=self.staff_id), str(self.place_dict[self.place]), str(self.in_out_dict[self.in_out])
     
 class Fee(SubmitAttendance):
     
